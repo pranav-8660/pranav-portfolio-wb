b47401feb81ea4644506096cdb55efd85812d93b
pranav-8660 <pranavcse2003@gmail.com>
2025-03-09 20:15:00 +0530

    changes the link for resume

--- src/App.tsx
+++ src/App.tsx
@@ -123,7 +123,7 @@ function App() {
                   <ChevronRight size={16} className="ml-2" />
                 </a>
                 <a
-                  href="https://drive.google.com/file/d/1NOLNNsQhHJW4_8c5ES5IRgk6mlfzhdVf/view?usp=drivesdk"  // Replace with actual resume link
+                  href="https://drive.google.com/file/d/1oeFDxWUiKevGchn5pNkw3UbgXr9HexuE/view"  
                   target="_blank"
                   rel="noopener noreferrer"
                   className="inline-flex items-center px-6 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-800 bg-opacity-60 hover:bg-opacity-70 transition-colors"
